bc9aedb0afe3eee8b774361f7b15d35d030d9011
Romain ROUFFET <rom1.rouffet@gmail.com>
2025-11-10 22:54:43 +0100

    Improve terrain gradient color consistency

--- shaders/_prelude.fragment.glsl
+++ shaders/_prelude.fragment.glsl
@@ -1,5 +1,11 @@
 #ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+precision highp int;
+#else
 precision mediump float;
+precision mediump int;
+#endif
 #else
 
 #if !defined(lowp)
