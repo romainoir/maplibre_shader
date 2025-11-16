c555e6d1bce02a2879b270501edbf612d4cf1eb8
Romain ROUFFET <rom1.rouffet@gmail.com>
2025-11-16 20:36:37 +0100

    Ensure sky renders atop custom layers

--- render/draw_sky.ts
+++ render/draw_sky.ts
@@ -45,7 +45,10 @@ export function drawSky(painter: Painter, sky: Sky) {
 
     const skyUniforms = skyUniformValues(sky, painter.style.map.transform, painter.pixelRatio);
 
-    const depthMode = new DepthMode(gl.LEQUAL, DepthMode.ReadWrite, [0, 1]);
+    // Always draw the sky regardless of existing depth values so custom layers
+    // cannot occlude it. Avoid modifying the depth buffer to preserve
+    // information for subsequent rendering steps (e.g. atmosphere).
+    const depthMode = new DepthMode(gl.ALWAYS, DepthMode.ReadOnly, [0, 1]);
     const stencilMode = StencilMode.disabled;
     const colorMode = painter.colorModeForRenderPass();
     const program = painter.useProgram('sky');
